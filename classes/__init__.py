291a2aecb6adda583881274d0463894abc1b6de8
Rodrigo Alves <rodrigoalvys@gmail.com>
2024-11-21 21:49:51 -0300

    Classes de personalidade

---
+++ classes/__init__.py
@@ -0,0 +1,87 @@
+from random import randint
+
+class base():
+    tipo = ""
+    text = ""
+    nome = "nome"
+    pontos_fortes = []
+    pontos_fracos = []
+    def get_tipo(self):
+        return self.tipo
+    def get_text(self):
+        return self.text
+    def get_nome(self):
+        return self.nome
+    def get_positivos(self):
+        return self.pontos_fortes
+    def get_negativos(self):
+        return self.pontos_fracos
+    def teste(self):
+        print(f"{self.get_tipo()}:{self.get_nome()}\n{self.get_text()}\n\nPontos forte:")
+        for x in self.get_positivos():
+            print("\n" + x)
+        print("\nPontos fracos:")
+        for x in self.get_negativos():
+            print("\n" + x)
+
+
+class Analista(base):
+    tipo = "Analista"
+    def __init__(self):
+        x = randint(1, 5)
+        with open("banco_de_dados/" + self.tipo + ".txt", 'r') as arquivo:
+            conteudo = arquivo.read()
+        textos = conteudo.split("Texto ")
+        partes = textos[x].split("\nPontos Positivos:\n")
+        caracte = partes[1].split("\nPontos Negativos:\n")
+        texto = partes[0].strip()
+        self.pontos_fortes = caracte[0].strip().split("\n")
+        self.pontos_fracos = caracte[1].strip().split("\n")
+        self.nome = texto.splitlines()[1]
+        self.text = texto.splitlines()[2]
+
+class Diplomata(base):
+    tipo = "Diplomata"
+    def __init__(self):
+        x = randint(1, 5)
+        with open("banco_de_dados/" + self.tipo + ".txt", 'r') as arquivo:
+            conteudo = arquivo.read()
+        textos = conteudo.split("Texto ")
+        partes = textos[x].split("\nPontos Positivos:\n")
+        caracte = partes[1].split("\nPontos Negativos:\n")
+        texto = partes[0].strip()
+        self.pontos_fortes = caracte[0].strip().split("\n")
+        self.pontos_fracos = caracte[1].strip().split("\n")
+        self.nome = texto.splitlines()[1]
+        self.text = texto.splitlines()[2]
+
+class Sentinelas(base):
+    tipo = "Sentinelas"
+    def __init__(self):
+        x = randint(1, 5)
+        with open("banco_de_dados/" + self.tipo + ".txt", 'r') as arquivo:
+            conteudo = arquivo.read()
+        textos = conteudo.split("Texto ")
+        partes = textos[x].split("\nPontos Positivos:\n")
+        caracte = partes[1].split("\nPontos Negativos:\n")
+        texto = partes[0].strip()
+        self.pontos_fortes = caracte[0].strip().split("\n")
+        self.pontos_fracos = caracte[1].strip().split("\n")
+        self.nome = texto.splitlines()[1]
+        self.text = texto.splitlines()[2]
+
+class Exploradores(base):
+    tipo = "Exploradores"
+    def __init__(self):
+        x = randint(1, 5)
+        with open("banco_de_dados/" + self.tipo + ".txt", 'r') as arquivo:
+            conteudo = arquivo.read()
+        textos = conteudo.split("Texto ")
+        partes = textos[x].split("\nPontos Positivos:\n")
+        caracte = partes[1].split("\nPontos Negativos:\n")
+        texto = partes[0].strip()
+        self.pontos_fortes = caracte[0].strip().split("\n")
+        self.pontos_fracos = caracte[1].strip().split("\n")
+        self.nome = texto.splitlines()[1]
+        self.text = texto.splitlines()[2]
+
